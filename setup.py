5ea265c099096b773bd36c56539c6f4ce62c00c9
Laurent Courty <lrntct@gmail.com>
2017-12-02 23:00:34 -0600

    use absolute path to locate source files

--- setup.py
+++ setup.py
@@ -42,8 +42,9 @@ def get_description():
 
 
 # get a list of SWMM source files
-SWMM_SOURCE = os.path.join('pyswmm', 'owaswmm', 'src')
-SWMM_INCLUDES = [SWMM_SOURCE, os.path.join('pyswmm', 'owaswmm', 'include')]
+OWASWMM_PATH = os.path.join(HERE, 'pyswmm', 'owaswmm')
+SWMM_SOURCE = os.path.join(OWASWMM_PATH, 'src')
+SWMM_INCLUDES = [SWMM_SOURCE, os.path.join(OWASWMM_PATH, 'include')]
 
 def swmm_get_source():
     """locate and return a list of source files
